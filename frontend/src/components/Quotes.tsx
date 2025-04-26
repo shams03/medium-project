c27fc98272ed21bb6a6f362bccfd6a505ede1968
shams03 <95904933+shams03@users.noreply.github.com>
2025-04-27 01:25:17 +0530

    made ui backend part and cloud config remaining

--- frontend/src/components/Quotes.tsx
+++ frontend/src/components/Quotes.tsx
@@ -1,7 +1,12 @@
-   
-
 const Quotes = () => {
-  return <div>Hi from quotes</div>;
+  return (
+    <div className="text-center mt-10 px-4 py-6 bg-muted rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold text-primary">medium.com</h1>
+      <p className="mt-4 italic text-lg font-semibold text-foreground/80">
+        “Words are free. It’s how you use them that may cost you.”
+      </p>
+    </div>
+  );
 };
 
 export default Quotes;
